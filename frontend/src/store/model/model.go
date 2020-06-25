b2676aaa8d480db393591cb5010faae2af6616ec
Sea'n Murphy <sean@gopaddy.ch>
2020-06-25 16:44:57 +0200

    Added initial frontend

---
+++ frontend/src/store/model/model.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Item represents a single TODO item in the store.
+type Item struct {
+	ID           uuid.UUID
+	Title        string
+	Completed    bool
+	CreationDate time.Time
+}
+
+// FilterState represents a viewing filter for TODO items in the store.
+type FilterState int
+
+const (
+	// All is a FilterState which shows all items.
+	All FilterState = iota
+
+	// Active is a FilterState which shows only non-completed items.
+	Active
+
+	// Completed is a FilterState which shows only completed items.
+	Completed
+)
---
+++ frontend/src/todo-client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gopherjs/vecty"
+	"github.com/seanrmurphy/go-fullstack/frontend/src/components"
+	"github.com/seanrmurphy/go-fullstack/frontend/src/store"
+)
+
+var (
+	RestEndpoint = "https://3t9ljow8x2.execute-api.eu-west-1.amazonaws.com/prod/"
+)
+
+func main() {
+
+	vecty.SetTitle("GopherJS • TodoMVC")
+	vecty.AddStylesheet("https://rawgit.com/tastejs/todomvc-common/master/base.css")
+	vecty.AddStylesheet("https://rawgit.com/tastejs/todomvc-app-css/master/index.css")
+
+	store.Initialize(RestEndpoint)
+	log.Printf("store.items length = %v\n", len(store.Items))
+
+	p := &components.PageView{}
+	store.Listeners.Add(p, func() {
+		p.Items = store.Items
+		log.Printf("in func p.items length = %v\n", len(p.Items))
+		vecty.Rerender(p)
+	})
+	log.Printf("p.items length = %v\n", len(p.Items))
+	vecty.RenderBody(p)
+}
